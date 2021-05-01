c4e9091d11c449991151b962b0f7f757b9c356ae
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2021-05-01 18:07:04 +0200

    Pagination added to the blog view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render,redirect,reverse
 from .models import Post
 from marketing.models import Signup
@@ -25,8 +26,20 @@ def index(request):
 
 def blog(request):
     post_list = Post.objects.all()
+    paginator = Paginator(post_list, 1)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
     context = {
-        "post_list":post_list
+        "queryset":paginated_queryset,
+        "page_request_var":page_request_var
     }
     return render(request, "blog.html" , context)
 
